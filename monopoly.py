e1cc651267cf6e3be533a8d986706bc2844ce0d4
Azhar Khan <akhan023@student.wethinkcode.co.za>
2023-11-03 14:51:01 +0200

    added menu and places

--- monopoly.py
+++ monopoly.py
@@ -1,6 +1,5 @@
 import radice as rd
 
-
 def monopoly():
     pass 
 
@@ -8,7 +7,22 @@ def run_game():
     pass
 
 def menu():
-    pass
+    options = {1: "Add Player", 2: "Start Game"}
+    
+    for option in options:
+        print("{} - {}".format(option, options[option]))
+    
+    player_input(options)
+    
+    print(options[option])
+def player_input(options):
+    option = input("What would you like to do? ")
+    
+    if int(option) in options:
+        return option
+    else:
+        print("Invalid Option")
+        return player_input(options)
 
 if __name__ == "__main__":
-    pass
+    menu()
--- places.py
+++ places.py
@@ -1,7 +1,71 @@
 #places with prices
 
 def place():
-    pass
+    
+    Board = [
+            Tile("GO"),
+            Property("Mediterranean Avenue", 60, 2),
+            Tile("Community Chest"),
+            Property("Baltic Avenue",60, 8),
+            Tile("Income Tax"),
+            Property("Reading Railroad", 200, 50),
+            Property("Oriental Avenue", 100, 6),
+            Tile("Chance"),
+            Property("Vermont Avenue", 100, 6),
+            Property("Connecticut Avenue", 120, 8),
+            Tile("Jail"),
+            Property("St. Charles Place", 140, 10),
+            Utility("Electric Company", 150, 1),
+            Property("States Avenue", 140, 10),
+            Property("Virginia Avenue", 160, 12),
+            Property("Pennsylvania Railroad", 200, 50),
+            Property("St. James Place", 180, 14),
+            Community("Community Chest"),
+            Property("Tennessee Avenue", 180, 14),
+            Property("New York Avenue", 200, 16),
+            Tile("Free Parking"),
+            Property("Kentucky Avenue", 220, 18),
+            Tile("Chance"),
+            Property("Indiana Avenue", 220, 18),
+            Property("Illinois Avenue", 240, 20),
+            Property("B. & O. Railroad", 200, 50),
+            Property("Atlantic Avenue", 260, 22),
+            Property("Ventnor Avenue", 260, 22),
+            ("Water Works", 150, 0),
+            Property("Marvin Gardens", 280, 24),
+            Jail("Go To Jail"),
+            Property("Pacific Avenue", 300, 26),
+            Property("North Caroliina Avenue", 300, 26),
+            Tile("Community Chest"),
+            Property("Pennsylvania Avenue", 320, 28),
+            Property("Short Line", 200, 50),
+            Chance("Chance"),
+            Property("Park Place", 350, 35),
+            Tile("Luxury Tax", -100),
+            Property("Boardwalk", 400, 50)]
 
-def place_rent():
-    pass
+def Property(name_property, price_of_property):
+    
+    return name_property, price_of_property
+    
+def Tile(name):
+    
+    return name
+
+def Utility(place):
+    
+    return place
+
+def Chance(name):
+    
+    return name
+
+def Community(name):
+    
+    return name
+
+def Jail(call):
+    return call
+
+def Owned(players, owned_cards):
+    return True
