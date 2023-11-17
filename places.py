718b9ffd5d168ffc66ebaea1b93b5d546d173a0b
Azhar Khan <akhan023@student.wethinkcode.co.za>
2023-11-17 16:19:28 +0200

    Missed One Property

--- places.py
+++ places.py
@@ -4,7 +4,7 @@ def place():
     # board = [
         # Property("OLD KENT ROAD", 60, 70)]
     Board = {
-        0: {"Name": "Go", "Mortgage": 200},
+    0: {"Name": "Go", "Mortgage": 200},
     1: {"Name": "OLD KENT ROAD", "Mortgage": 60, "Levels": (70, 130, 220, 370, 750)},
     2: {"Name": "EVENT TILE", "Mortgage": 0},
     3: {"Name": "WHITECHAPEL ROAD", "Mortgage": 60},
@@ -12,12 +12,13 @@ def place():
     5: {"Name": "THE ANGEL, ISLINGTON", "Mortgage": 100},
     6: {"Name": "EUSTON ROAD", "Mortgage": 100},
     7: {"Name": "LOCATION", "Mortgage": 100},
-    9: {"Name": "PENTONVILLE ROAD", "Mortgage": 120},
-    10: {"Name": "JAIL", "Mortgage": 50},
-    11: {"Name": "PALL MALL", "Mortgage": 140},
-    12: {"Name": "EVENT TILE", "Mortgage": 0},
-    13: {"Name": "WHITE HALL", "Mortgage": 140},
-    14: {"Name": "NORTHUMB'ND AVENUE", "Mortgage": 160},
+    8: {"Name": "PENTONVILLE ROAD", "Mortgage": 120},
+    9: {"Name": "JAIL", "Mortgage": 50},
+    10: {"Name": "PALL MALL", "Mortgage": 140},
+    11: {"Name": "EVENT TILE", "Mortgage": 0},
+    12: {"Name": "WHITE HALL", "Mortgage": 140},
+    13: {"Name": "NORTHUMB'ND AVENUE", "Mortgage": 160},
+    14: {"Name": "BOW STREET", "Mortgage": 180},
     15: {"Name": "MARLBOROUGH STREET", "Mortgage": 180},
     16: {"Name": "LOCATION", "Mortgage": 100},
     17: {"Name": "VINE STREET", "Mortgage": 200},
@@ -39,7 +40,7 @@ def place():
     33: {"Name": "PARK LANE", "Mortgage": 350},
     34: {"Name": "LOCATION", "Mortgage": 100},
     35: {"Name": "MAYFAIR", "Mortgage": 400},
-    }
+}
 
            
 def Property(name_property, price_of_property, Rent_level_1):
